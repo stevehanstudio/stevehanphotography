fad8839456d23e57e0ad4d32964b9a1d16227460
Steve Han <15229911+stevehanphoto@users.noreply.github.com>
2020-09-03 10:58:24 -0700

    Resize footer height

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,19 +14,23 @@ const Wrapper = styled.footer`
   left: 0;
   bottom: 0;
   width: 100%;
-  height: 5rem;
+  height: 4.6rem;
   display: grid;
   place-items: center;
-  background: ${({ location }) => (location === '/' ? 'transparent' : 'var(--clr-black)')};
+  background: ${({ location }) =>
+    location === "/" ? "transparent" : "var(--clr-black)"};
   //background: transparent;
   //opacity: 0.8;
   //  background: var(--clr-black);
   text-align: center;
   padding: 1rem;
+  @media (max-width: 576px) {
+    height: 3.6rem;
+  }
   p {
     //color: var(--clr-white);
-    color: rgba(255,255,255,0.7);
-    text-shadow: 1px 1px 2px rgba(0,0,0,0.1);
+    color: rgba(255, 255, 255, 0.7);
+    text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.1);
     margin-bottom: 0;
     @media (max-width: 576px) {
       font-size: 0.75rem;
