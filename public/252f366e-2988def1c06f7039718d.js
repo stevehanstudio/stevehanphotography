f994c252d47b09937f7fe2f939e50beb2d38a1fc
Steve Han <15229911+stevehanphoto@users.noreply.github.com>
2020-09-23 02:23:14 -0700

    Removed public folder from .gitignore to fix Netlify deploy error

---
+++ public/252f366e-2988def1c06f7039718d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{NIcq:function(t,c,n){"use strict";n.d(c,"a",(function(){return i}));var a=n("Lnxd");function i(t){return Object(a.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M21 8V7l-3 2-3-2v1l3 2 3-2zm1-5H2C.9 3 0 3.9 0 5v14c0 1.1.9 2 2 2h20c1.1 0 1.99-.9 1.99-2L24 5c0-1.1-.9-2-2-2zM8 6c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm6 12H2v-1c0-2 4-3.1 6-3.1s6 1.1 6 3.1v1zm8-6h-8V6h8v6z"}}]})(t)}}}]);
+//# sourceMappingURL=252f366e-2988def1c06f7039718d.js.map
